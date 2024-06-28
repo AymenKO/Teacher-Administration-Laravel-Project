236f98d852ec65af25f07e43b06cac4664fbbdeb
AymenKO <aymen.kolsi01@gmail.com>
2024-06-28 10:15:16 +0100

    commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,10 +5,6 @@
         </h2>
     </x-slot>
 
-    <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)" class="fixed top-0 left-0 right-0 bg-green-500 text-white text-center p-4">
-        {{ __("You're logged in as an admin!") }}
-    </div>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -5,9 +5,6 @@
         </h2>
     </x-slot>
 
-    <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)" class="fixed top-0 left-0 right-0 bg-green-500 text-white text-center p-4">
-        {{ __("You're logged in as a teacher!") }}
-    </div>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
